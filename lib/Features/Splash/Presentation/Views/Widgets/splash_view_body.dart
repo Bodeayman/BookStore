e413685a863e003056f108b49d798872f3521658
Abdullah Ayman <abdulluhayman@gmail.com>
2025-01-21 17:58:25 +0200

    Added the sliding animation

--- lib/Features/Splash/Presentation/Views/Widgets/splash_view_body.dart
+++ lib/Features/Splash/Presentation/Views/Widgets/splash_view_body.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_practise/Features/Splash/Presentation/Views/Widgets/sliding_text.dart';
 import 'package:mvvm_practise/core/utils/assets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  // 0 --> 1
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        new AnimationController(vsync: this, duration: Duration(seconds: 1));
+    slidingAnimation = Tween<Offset>(begin: Offset(0, 2), end: Offset.zero)
+        .animate(_animationController);
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+    //This to prevent memory leaks
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image.asset(AssetsData.logo),
-          const SizedBox(height: 20),
-          const Text(
-            "Read Free Books",
-            textAlign: TextAlign.center,
-          )
-        ]);
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(height: 20),
+        Sliding_text(slidingAnimation: slidingAnimation)
+      ],
+    );
   }
 }
